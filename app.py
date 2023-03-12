fbc02b1af4b3be19dec55a80b7520bbf3d0e7040
S.V <ssv.vakhrusheva@gmail.com>
2023-03-12 18:54:53 +0200

    Application setup

--- app.py
+++ app.py
@@ -8,35 +8,40 @@ def load():
 
 # gender code
 def gender_code(gender):
-    gdict = {'man': 0, 'woman': 1}
+    gdict = {'male': 0, 'female': 1}
 
     return gdict[gender]
 
+# headings
+st.header('Check your risk of heart diseases')
+st.write('Enter the required information below to receive the forecast.')
 
 # interface for input parameters
-age = st.slider('Enter your age:', 18, 100)
-weight = st.slider('Enter your weight:',
-                   40, 120)
-height = st.slider('Enter your height:', 150, 200)
+col1, col2 = st.columns(2)
 
-ap_hi = st.slider('Enter systolic blood pressure value:', 24, 240)
-ap_lo = st.slider('Enter diastolic blood pressure value:', 15, 150)
+with col1:
+    st.subheader('General information:')
+    gender = st.selectbox('Select your gender:', ['male', 'female'])
+    age = st.slider('Enter your age:', 18, 100)
+    weight = st.slider('Enter your weight:', 40, 120)
+    height = st.slider('Enter your height:', 150, 200)
+    smoke = st.checkbox('Do you smoke?')
+    alko = st.checkbox('Do you drink alcohol?')
+    active = st.checkbox('Do you lead an active lifestyle?')
 
-gender = st.selectbox('Select your gender:', ['man', 'woman'])
-cholesterol = st.selectbox('Select your cholesterol level:', [1, 2, 3])
-gluc = st.selectbox('Select your glucose level:', [1, 2, 3])
+with col2:
+    st.subheader('Medical indicators:')
+    ap_hi = st.slider('Enter systolic blood pressure value:', 24, 240)
+    ap_lo = st.slider('Enter diastolic blood pressure value:', 15, 150)
+    st.write(':red[WARNING: Diastolic pressure must be larger systolic pressure.]')
+    cholesterol = st.radio('Select your cholesterol level:', [1, 2, 3])
+    gluc = st.radio('Select your glucose level:', [1, 2, 3])
 
-smoke = st.checkbox('Do you smoke?')
-alko = st.checkbox('Do you drink alcohol?')
-active = st.checkbox('Do you practice aports?')
 
 # preprocessing
 bmi = weight / height**2
 ap_diff = ap_hi - ap_lo
 gender = gender_code(gender)
-
-if ap_lo > ap_hi:
-    st.write('WARNING: Diastolic pressure > systolic pressure. Check your data.')
 
 # params to list
 params = [age, gender, ap_hi, cholesterol, gluc,
@@ -44,5 +49,8 @@ params = [age, gender, ap_hi, cholesterol, gluc,
 
 # prediction
 model = load()
-y_predict = model.predict_proba([params])[:, 1]
-st.write(f'Risk of heart diseases: {round(y_predict, 2)}')
+y_predict = float(model.predict_proba([params])[:, 1])
+
+st.subheader(' ')
+st.subheader(f'Risk of heart diseases: {"{:.0%}".format(y_predict)}')
+st.write(':red[NOTE]:  Model prediction is not abolute and is advisory in nature. For more accurate diagnosis, please consult a specialist.')
